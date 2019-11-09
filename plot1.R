6d2f9dcf76868619403314efc9fcda91669d122d
Prashantchand <54577137+Prashantchand@users.noreply.github.com>
2019-11-09 15:20:24 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -8,7 +8,7 @@ if(!file.exists("household_power_consumption")){
   unzip(filename)
 }
 library(data.table)
-Powerdata<-read.table("household.txt",sep=";",header = TRUE,colClasses ="character",na.strings = "?")
+Powerdata<-read.table("household_power_consumption.txt",sep=";",header = TRUE,colClasses ="character",na.strings = "?")
 data<-Powerdata[Powerdata$Date=="2/2/2007"|Powerdata$Date=="1/2/2007",]
 data$Global_active_power<-as.numeric(data$Global_active_power)
 hist(data$Global_active_power,xlab = "Global Active Power(kilowatts)",main ="Global Active Power",col = "red")
